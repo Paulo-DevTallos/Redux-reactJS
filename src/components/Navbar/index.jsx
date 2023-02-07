60413099b2999aed1f5054d6b67ca86fdc0d507d
Paulo Sergio <pauloserg861@gmail.com>
2023-02-07 08:33:37 -0300

    criando navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,37 @@
+import styles from './Navbar.module.scss';
+import classNames from 'classnames';
+
+import { RiShoppingCart2Line, RiShoppingCartFill } from 'react-icons/ri';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+const iconProps = {
+  color: 'white',
+  size: 24
+}
+
 const Navbar = () => {
   return (
-    <nav>
-      navbar
+    <nav className={styles.nav}>
+      <Logo className={styles.logo} />
+      <div className={styles.links}>
+        <div>
+          {/* [representa a hash que o css module gera]: */}
+          <a href="/" className={classNames(styles.link, {
+            [styles.selected]: window.location.pathname === '/'
+          })}>Página inicial</a>
+        </div>
+      </div>
+      <div className={styles.busca}>
+
+      </div>
+      <div className={styles.icones}>
+        <a href="/carrinho">
+          { window.location.pathname === '/carrinho'
+            ? <RiShoppingCartFill {...iconProps} />
+            : <RiShoppingCart2Line {...iconProps  }/>
+          }
+        </a>
+      </div>
     </nav>
   );
 }
